4056a147bafd89f24620554143fd824496bd1d92
innocent-maina <innocentwahome@gmail.com>
2025-02-10 19:49:58 +0300

    update theme settings & menu styling

--- components/Navigation/Menu.vue
+++ components/Navigation/Menu.vue
@@ -1,46 +1,3 @@
-<template>
-  <div id="main-menu" class="wsmainfull menu clearfix">
-    <div class="wsmainwp clearfix">
-      <!-- HEADER BLACK LOGO -->
-      <div class="desktoplogo">
-        <NuxtLink to="/" class="logo-black"><img style="min-height: 86px; width: 117px; padding: 5px;"
-            src="/assets/images/DCLogo.png" alt="logo" /></NuxtLink>
-      </div>
-      <!-- HEADER WHITE LOGO -->
-      <div class="desktoplogo">
-        <NuxtLink to="/" class="logo-white"><img style="min-height: 61px; width: 85px" src="/assets/images/DCLogo.png"
-            alt="logo" /></NuxtLink>
-      </div>
-      <!-- MAIN MENU -->
-      <nav class="wsmenu clearfix" style="
-          display: flex;
-          justify-content: center;
-          align-content: center;
-          align-items: center;
-        ">
-        <ul class="wsmenu-list nav-theme">
-          <li class="nl-simple" aria-haspopup="true">
-            <NuxtLink to="/about-us" class="h-link">About Us</NuxtLink>
-          </li>
-          <li class="nl-simple" aria-haspopup="true">
-            <NuxtLink to="/career-coaching" class="h-link">Career Coaching</NuxtLink>
-          </li>
-          <li class="nl-simple" aria-haspopup="true">
-            <NuxtLink to="/virtual-assistance" class="h-link">Virtual Assistance</NuxtLink>
-          </li>
-          <li class="nl-simple" aria-haspopup="true">
-            <NuxtLink to="/contact-us" class="h-link">Contact Us</NuxtLink>
-          </li>
-          <li class="nl-simple pr-5" aria-haspopup="true">
-            <NuxtLink to="/schedule-consultation" class="btn r-04 btn--theme hover--tra-white last-link">Schedule a Consultation
-            </NuxtLink>
-          </li>
-        </ul>
-      </nav>
-      <!-- END MAIN MENU -->
-    </div>
-  </div>
-</template>
 <script>
 import { reactive } from "vue";
 export default {
@@ -77,3 +34,47 @@ export default {
   },
 };
 </script>
+
+<template>
+  <div id="main-menu" class="wsmainfull menu clearfix">
+    <div class="wsmainwp clearfix">
+      <!-- HEADER BLACK LOGO -->
+      <div class="desktoplogo">
+        <NuxtLink to="/" class="logo-black"><img style="min-height: 86px; width: 117px; padding: 5px;"
+            src="/assets/images/DCLogo.png" alt="logo" /></NuxtLink>
+      </div>
+      <!-- HEADER WHITE LOGO -->
+      <div class="desktoplogo">
+        <NuxtLink to="/" class="logo-white"><img style="min-height: 61px; width: 85px" src="/assets/images/DCLogo.png"
+            alt="logo" /></NuxtLink>
+      </div>
+      <!-- MAIN MENU -->
+      <nav class="wsmenu clearfix" style="
+          display: flex;
+          justify-content: center;
+          align-content: center;
+          align-items: center;
+        ">
+        <ul class="wsmenu-list nav-theme">
+          <li class="nl-simple custom-menu-items"  aria-haspopup="true">
+            <NuxtLink to="/about-us" class="h-link">About Us</NuxtLink>
+          </li>
+          <li class="nl-simple custom-menu-items" aria-haspopup="true">
+            <NuxtLink to="/career-coaching" class="h-link">Career Coaching</NuxtLink>
+          </li>
+          <li class="nl-simple custom-menu-items" aria-haspopup="true">
+            <NuxtLink to="/virtual-assistance" class="h-link">Virtual Assistance</NuxtLink>
+          </li>
+          <li class="nl-simple custom-menu-items" aria-haspopup="true">
+            <NuxtLink to="/contact-us" class="h-link">Contact Us</NuxtLink>
+          </li>
+          <li class="nl-simple pr-5" aria-haspopup="true">
+            <NuxtLink to="/schedule-consultation" class="btn r-04 btn--theme hover--tra-white last-link">Schedule a Consultation
+            </NuxtLink>
+          </li>
+        </ul>
+      </nav>
+      <!-- END MAIN MENU -->
+    </div>
+  </div>
+</template>
