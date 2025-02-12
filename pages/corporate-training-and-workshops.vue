466d6eab089903cb74780a61f5b69fa494aa4c56
innocent-maina <innocentwahome@gmail.com>
2025-02-12 11:08:36 +0300

    refactor location of demopages

--- pages/corporate-training-and-workshops.vue
+++ pages/corporate-training-and-workshops.vue
@@ -10,29 +10,29 @@ useSeoMeta({
 
 useHead({
   bodyAttrs: {
-    class: "navbar-dark navbar-dark-2",
-  },
+      class: "navbar-dark scheme-magenta"
+  }
 });
 </script>
 
 <template>
   <div>
-    <!-- Header-->
-    <SectionsCareerPageHeader />
-
-    <!-- Pricing cards -->
-    <SectionsCareerPageMainPricing />
-
-    <!-- Bundled pricing / Accepted payments -->
-    <SectionsCareerPageBundledPricing />
-
-    <!-- Employees have worked here... -->
-    <SectionsCareerPageEmployeesWork />
-
-    <!-- Clients rating & Testimonials -->
-    <SectionsCareerPageTestimonialsCards />
-
-    <!-- Q/A -->
-    <SectionsCareerPageQA />
+      <SectionsDemo13Section1 />
+      <SectionsDemo13Section2 />
+      <SectionsDemo13Section3 />
+      <SectionsDemo13Section4 />
+      <SectionsDemo13Section5 />
+      <SectionsDemo13Section6 />
+      <SectionsDemo13Section7 />
+      <SectionsDemo13Section8 />
+      <SectionsDemo13Section9 />
+      <SectionsDemo13Section10 />
+      <SectionsDemo13Section11 />
+      <SectionsDemo13Section12 />
+      <SectionsDemo13Section13 />
+      <SectionsDemo13Section14 />
+      <SectionsDemo13Section15 />
+      <SectionsDemo13Section16 />
+      <SectionsDemo13Section17 />
   </div>
 </template>
